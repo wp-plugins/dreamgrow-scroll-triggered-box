87509bef58eb71b8a3176f612d0af92a57d3a70a
pk2000 <pk2000@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-18 13:16:24 +0000

    Initial commit
    git-svn-id: https://plugins.svn.wordpress.org/dreamgrow-scroll-triggered-box/trunk@559904 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require (ABSPATH . WPINC . '/pluggable.php');
 Plugin Name: Scroll Triggered Box
 Plugin URI: http://dreamgrow.com
 Description: Scroll Triggered Box...
-Version: 0.9.4
+Version: 1
 Author: Dreamgrow Digital
 Author URI: http://dreamgrow.com
 License: GPL2
