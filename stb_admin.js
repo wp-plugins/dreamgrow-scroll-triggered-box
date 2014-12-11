4cb4c6e8fa728537cdefeddc55fdae99d412c570
Eero Hermlin <Eero Hermlin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-11 13:22:57 +0000

    Remove olve version files from 2.0
    git-svn-id: https://plugins.svn.wordpress.org/dreamgrow-scroll-triggered-box/tags/2.0@1042686 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- stb_admin.js
+++
@@ -1,9 +0,0 @@
-jQuery(function() {
-    jQuery( "#tabs" ).tabs();
-	
-	jQuery('#cleanCookie').click(function(){
-		jQuery.removeCookie('nopopup',{path: '/' });
-		window.location.href = window.location.href;
-		return false;
-	});
-});
--- uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
-        exit();
-    function uninstall(){
-        delete_option( 'stb_settings' );
-        delete_option( 'stb_html' );
-    }
-    uninstall();
-?>
